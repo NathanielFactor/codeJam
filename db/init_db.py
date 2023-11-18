a82d713f75460108e67d019d9f7d24d4acb9546e
ray-bao-mcgill <ray.bao@mail.mcgill.ca>
2023-11-18 04:32:02 -0500

    update 2.0

--- db/init_db.py
+++ db/init_db.py
@@ -8,13 +8,6 @@ with open('schema.sql') as f:
 
 cur = connection.cursor()
 
-cur.execute("INSERT INTO ingredients (ingredient, expiry, amount) VALUES (?, ?, ?)",
-            ('First ingredient', 0, 1)
-            )
-
-cur.execute("INSERT INTO ingredients (ingredient, expiry, amount) VALUES (?, ?, ?)",
-            ('Second ingredient', 0, 1)
-            )
 
 connection.commit()
 connection.close()
